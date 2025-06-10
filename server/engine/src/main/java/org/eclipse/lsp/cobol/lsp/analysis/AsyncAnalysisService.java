66b48ec0ec57e8ebb9feffa5f8232c8817c285c8
amanprashant <aman.prashant@broadcom.com>
2025-06-10 11:14:58 +0200

    fix: cancel  prev running analysis unconditioned, in case of a new request
    Signed-off-by: amanprashant <aman.prashant@broadcom.com>

--- server/engine/src/main/java/org/eclipse/lsp/cobol/lsp/analysis/AsyncAnalysisService.java
+++ server/engine/src/main/java/org/eclipse/lsp/cobol/lsp/analysis/AsyncAnalysisService.java
@@ -149,10 +149,7 @@ public class AsyncAnalysisService implements AnalysisStateNotifier {
     }
     ExecutorService analysisExecutor = getExecutor(uri);
     CobolDocumentModel documentModel = documentModelService.get(uri);
-    if (documentModel.getLastAnalysisResult() != null) {
-      // initiate cancel on prev running analysis
-      cancelRunningAnalysis(ImmutableList.of(documentModel));
-    }
+    cancelRunningAnalysis(ImmutableList.of(documentModel));
     FutureTask<CobolDocumentModel> futureTask =
         new FutureTask<>(
             scheduleAnalysis(uri, text, currentRevision, open, force, eventSource, id));
