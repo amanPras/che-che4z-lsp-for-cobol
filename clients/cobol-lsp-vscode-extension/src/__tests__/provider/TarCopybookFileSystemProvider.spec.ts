82714198e5c4d909415e9995080b09ddc08b6881
Kutlu <kutlu.ozel@broadcom.com>
2026-01-16 11:03:33 +0100

    weird try

--- clients/cobol-lsp-vscode-extension/src/__tests__/provider/TarCopybookFileSystemProvider.spec.ts
+++ clients/cobol-lsp-vscode-extension/src/__tests__/provider/TarCopybookFileSystemProvider.spec.ts
@@ -20,7 +20,7 @@ describe("TarCopybookFileSystemProvider", () => {
   const root = vscode.Uri.joinPath(vscode.Uri.parse(__dirname), "../../");
   const testFileUri = vscode.Uri.joinPath(
     root,
-    "src",
+    root.fsPath.includes("src") ? "" : "src",
     "__tests__",
     "resources",
     "tar",
